4ae10d80e557f274c03f0747df5501aabaf05458
STANLEY NGUMA <stanley.nguma@riverbank.co.ke>
2016-05-13 16:26:16 +0300

    Node Http Request

---
+++ app.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var postData = '{"username":"sasa","password":"Nevada@123"}'
+
+var options = {
+  hostname: 'countytest.riverbank.co.ke',
+  port: 80,
+  path: '/api/mpesa_validation',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': postData.length
+  }
+};
+
+var req = http.request(options, (res) => {
+
+  var responseCode = res.statusCode;
+
+  switch (responseCode) {
+    case 200:
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.')
+    });
+
+    case 404:
+    console.log('Page not found');
+    break;
+
+    case 500:
+    console.log('An error has occured');
+    break;
+    default:
+    console.log('Could not Get the response code');
+    break;
+
+  }
+
+});
+
+req.on('error', (e) => {
+  console.log(`problem with request: ${e.message}`);
+});
+
+// write data to request body
+req.write(postData);
+req.end();
